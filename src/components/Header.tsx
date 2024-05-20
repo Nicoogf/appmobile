af85d2fa9b0582eb83cb4cba2299284d2fff6375
Nicolas <nicolasgfalabella@gmail.com>
2024-05-20 08:12:23 -0300

    Profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Logo } from './Logo'
 import Link from 'next/link'
 import { menu } from '@/assets/data'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import defaultImage from "../../public/default.jpg"
 
 const Header = () => {
   return (
@@ -11,11 +14,11 @@ const Header = () => {
         </section>
         <section> 
         <nav>
-          <ul className='bg-background-foreground flex items-center gap-x-1 p-1 rounded-full'>
-            
+          <ul className='bg-background-foreground flex items-center gap-x-1 p-1 rounded-4xl'>
+
             {menu.map((menu)=>(
                 <li key={menu.href}>
-                <Link href={menu.href} className='flex items-center gap-x-2 text-sm py-3 px-6 rounded-full transition-colors'> 
+                <Link href={menu.href} className={cn('flex items-center gap-x-2 text-sm py-3 px-6 rounded-4xl transition-colors' , menu.href === "/menu" ? "bg-primary font-medium" : "hover:bg-red-400")}> 
                   <i className={menu.icon}/>
                   <p> {menu.label} </p>
                 </Link>
@@ -26,7 +29,19 @@ const Header = () => {
           </ul>
         </nav>
         </section>
-        <section> User  </section>
+        <section> 
+        <ul className='flex items-center gap-x-3 bg-background-foreground p-3 rounded-4xl'>
+          <li className='flex items-center gapx-2'>
+            <Image src={defaultImage} alt="Image" width={100} height={100} className='w-10 h-10 object-cover rounded-4xl' />
+            <h5 className='font-medium'> User_default </h5>
+          </li>
+          <li className='flex items-center gapx-2'>         
+            <button className='bg-background text-xl w-10 h-10 flex items-center justify-center rounded-4xl'>
+              <i className='fi fi-rr-settings'> </i>
+            </button>
+          </li>
+        </ul>
+        </section>
     </header>
   )
 }
